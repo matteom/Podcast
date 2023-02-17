ed866b48f31bc1dff7b7fd17062cbf184e2fa9b6
Matteo Manferdini <matteo@purecreek.com>
2023-02-17 15:22:14 +0100

    Initial Commit

---
+++ Podcast/PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastApp.swift
+//  Podcast
+//
+//  Created by Matteo Manferdini on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
