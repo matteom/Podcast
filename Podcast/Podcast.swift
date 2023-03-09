38dd4871497de4fcfba1076f6752669a9b8cf251
Matteo Manferdini <matteo@purecreek.com>
2023-03-09 20:57:01 +0100

    Add Podcast model type with JSON decoding

---
+++ Podcast/Podcast.swift
@@ -0,0 +1,45 @@
+//
+//  Podcast.swift
+//  Podcast
+//
+//  Created by Matteo Manferdini on 17/02/23.
+//
+
+import Foundation
+
+struct Podcast {
+	let id: Int
+	let title: String
+	let artist: String
+	let imageURL: URL
+	var episodes: [Episode]
+}
+
+extension Podcast: Decodable {
+	enum CodingKeys: String, CodingKey {
+		case id = "collectionId"
+		case title = "collectionName"
+		case artist = "artistName"
+		case imageURL = "artworkUrl600"
+	}
+
+	enum LookupCodingKeys: CodingKey {
+		case results
+	}
+
+	init(from decoder: Decoder) throws {
+		let lookupContainer = try decoder.container(keyedBy: LookupCodingKeys.self)
+		var resultsContainer = try lookupContainer.nestedUnkeyedContainer(forKey: LookupCodingKeys.results)
+		let podcastContainer = try resultsContainer.nestedContainer(keyedBy: CodingKeys.self)
+		self.id = try podcastContainer.decode(Int.self, forKey: .id)
+		self.title = try podcastContainer.decode(String.self, forKey: .title)
+		self.artist = try podcastContainer.decode(String.self, forKey: .artist)
+		self.imageURL = try podcastContainer.decode(URL.self, forKey: .imageURL)
+		var episodes: [Episode] = []
+		while !resultsContainer.isAtEnd {
+			let episode = try resultsContainer.decode(Episode.self)
+			episodes.append(episode)
+		}
+		self.episodes = episodes
+	}
+}
