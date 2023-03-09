6e97a5964e3852adee0b2c9abf76fca6a7c56428
Matteo Manferdini <matteo@purecreek.com>
2023-03-09 21:26:38 +0100

    Complete the podcast view with downloading, pausing, and resuming

--- Podcast/ContentView.swift
+++ Podcast/ContentView.swift
@@ -8,19 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@StateObject private var viewModel = ViewModel()
+
+	var body: some View {
+		List {
+			Header(podcast: viewModel.podcast)
+			if let podcast = viewModel.podcast {
+				ForEach(podcast.episodes) { episode in
+					EpisodeRow(episode: episode) {
+						toggleDownload(for: episode)
+					}
+				}
+			} else {
+				ForEach(0..<5) { _ in
+					EpisodeRow(episode: nil, onButtonPressed: {})
+				}
+			}
+		}
+		.listStyle(.plain)
+		.task { try? await viewModel.fetchPodcast() }
+	}
+}
+
+private extension ContentView {
+	func toggleDownload(for episode: Episode) {
+		if episode.isDownloading {
+			viewModel.pauseDownload(for: episode)
+		} else {
+			if episode.progress > 0 {
+				viewModel.resumeDownload(for: episode)
+			} else {
+				Task { try? await viewModel.download(episode) }
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		NavigationStack {
+			ContentView()
+		}
+	}
 }
