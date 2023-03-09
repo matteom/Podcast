646b4d812d1153af218fc23a286f3a9788bf4286
Matteo Manferdini <matteo@purecreek.com>
2023-03-09 21:07:59 +0100

    Add a download session to the view model

--- Podcast/ViewModel.swift
+++ Podcast/ViewModel.swift
@@ -10,6 +10,11 @@ import Foundation
 class ViewModel: NSObject, ObservableObject {
 	@Published var podcast: Podcast?
 
+	private lazy var downloadSession: URLSession = {
+		let configuration = URLSessionConfiguration.default
+		return URLSession(configuration: configuration, delegate: nil, delegateQueue: .main)
+	}()
+
 	@MainActor
 	func fetchPodcast() async throws {
 		let url = URL(string: "https://itunes.apple.com/lookup?id=1386867488&media=podcast&entity=podcastEpisode&limit=5")!
